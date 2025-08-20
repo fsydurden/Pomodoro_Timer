ac72f9d45d38fd4c42393d5fb101734193b30f34
karthik <jakustove@gmail.com>
2025-08-20 19:50:22 +0530

    UI setup completed

--- main.py
+++ main.py
@@ -20,13 +20,29 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title("Pomodoro Timer")
-window.config(padx=100,pady=50)
+window.config(padx=100,pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(103,112, image=tomato_img)
-canvas.pack()
+canvas.create_image(100,112, image=tomato_img)
+canvas.create_text(100,130, text="00:00" ,fill="white",font=(FONT_NAME,35,"bold"))
+canvas.grid(column=1,row=1)
 
+label = tkinter.Label(text="Timer")
+label.config(fg=GREEN ,bg=YELLOW ,font=(FONT_NAME,50,"bold"))
+label.grid(column=1,row=0)
+
+start_button = tkinter.Button()
+start_button.config(text="Start", highlightthickness=0)
+start_button.grid(column=0,row=2)
+
+reset_button = tkinter.Button()
+reset_button.config(text="Reset", highlightthickness=0)
+reset_button.grid(column=2,row=2)
+
+tick_label = tkinter.Label()
+tick_label.config(text="✔️", fg=GREEN, bg=YELLOW)
+tick_label.grid(column=1,row=3)
 
 
 window.mainloop()
